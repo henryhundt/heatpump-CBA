7814866d951caa66b8308ba9d1215b2ac4eda694
tedcallon <tedcallon@gmail.com>
2022-03-05 14:42:24 -0600

    Update cleaning homewyse data.R

--- Installation costs/cleaning homewyse data.R
+++ Installation costs/cleaning homewyse data.R
@@ -2,11 +2,11 @@ setwd("~/Documents/GitHub/heatpump-CBA/Installation costs")
 
 files <- dir()[str_detect(dir(), ".csv")]
 
-for(i in files){
+for(i in 2:length(files)){
   temp <- read.csv(files[i])
   for(j in 2:9){
     temp[,j] <- as.numeric(str_remove_all(temp[,j], "[$,]"))
   }
-  write.csv(i, row.names = F)
+  write.csv(temp, file = files[i], row.names = F)
 }
 
