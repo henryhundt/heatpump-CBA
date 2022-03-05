532781721dc8ec82f11de4a982f4b4a1c59cd3eb
tedcallon <tedcallon@gmail.com>
2022-03-05 10:19:11 -0600

    Create turn temp into heating load by month.R
    Uses the base temperature data to create a file that can be used to weight heating fuel prices such that e.g. we don't use a simple mean of natural gas costs in a year when thinking about how much it costs to heat a house with NG.

---
+++ Temperature Data/turn temp into heating load by month.R
@@ -0,0 +1,28 @@
+setwd("~/Desktop/ENV 810/Data/Temp")
+
+library(dplyr)
+library(stringr)
+
+df <- read.csv("2020 15 year temp normals in WI by 11 HDD zones.csv")
+
+df$HDH <- 65-df$HLY.TEMP.NORMAL
+df <- mutate(df, HDH = ifelse(HDH < 0, 0, HDH))
+
+df <- group_by(df, month, zone)
+
+df <- summarise(df, HDH = sum(HDH))
+
+totals <- group_by(df, zone)
+totals <- summarise(totals, HDH = sum(HDH))
+
+df$proportion <- 0
+for(i in df$zone){
+  df[df$zone == i,"proportion"] <- df[df$zone == i,"HDH"]/as.double(totals[totals$zone == i,"HDH"])
+}
+
+df <- arrange(df, zone)
+
+write.csv(df, "HDD Proportions by Month by Climate Zone Using 2020 15 Year Normals.csv", row.names = F)
+
+
+
