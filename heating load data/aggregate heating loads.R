9fbfdde3156dc692179a75b0e68484b6f26acc12
tedcallon <tedcallon@gmail.com>
2022-03-31 17:27:00 -0500

    NREL energy load files
    NREL end-use energy load profile files for all counties in Wisconsin (only looking at single-family detached houses).
    
    The aggregate heating loads file shows how they the raw data is turned into monthly load values.
    
    Currently in the montecarlo, the monthly values are just added up to an annual value, but in later versions we will keep the monthly values as is and use the respective monthly electricity costs instead of a weighted annual average electricity cost.
    
    Will probably also add the typical meteorological year data that each county energy load is built off of and use that instead of the HDD zone strategy.

---
+++ heating load data/aggregate heating loads.R
@@ -0,0 +1,49 @@
+setwd("~/Documents/GitHub/heatpump-CBA/heating load data")
+
+library(dplyr)
+library(stringr)
+library(lubridate)
+
+files <- dir()[which(str_detect(dir(), ".csv"))]
+
+final <- NA
+units <- 0
+for(i in 1:length(files)){
+  temp <- read.csv(files[i])
+  units <- unique(temp$units_represented)
+  county <- unique(temp$in.county)
+  temp <- select(temp, timestamp, out.electricity.heating.energy_consumption,
+                 out.electricity.heating_supplement.energy_consumption,
+                 out.fuel_oil.heating.energy_consumption,
+                 out.natural_gas.heating.energy_consumption,
+                 out.propane.heating.energy_consumption,
+                 out.wood.heating.energy_consumption)
+  kwh_to_mmBTU <- 3412/10^6 #https://www.eia.gov/energyexplained/units-and-calculators/energy-conversion-calculators.php
+  ##convert all kwh to mmBTU
+  temp[,c(2:7)] <- lapply(temp[,c(2:7)], function(col){col*kwh_to_mmBTU})
+  ##convert all non-electricity fuels to final energy using assumed efficiencies
+  ##we assume the efficiecny of all electricity is 100%
+  temp$out.fuel_oil.heating.energy_consumption <- temp$out.fuel_oil.heating.energy_consumption*.9
+  temp$out.natural_gas.heating.energy_consumption <- temp$out.natural_gas.heating.energy_consumption*.95
+  temp$out.propane.heating.energy_consumption <- temp$out.propane.heating.energy_consumption*.90
+  temp$out.wood.heating.energy_consumption <- temp$out.wood.heating.energy_consumption*.69
+  ##take sum of mmBTU by row
+  temp$heating_load <- apply(temp[,c(2:7)], 1, function(row){sum(row)})
+  temp$month <- month(temp$timestamp)
+  temp <- group_by(temp, month)
+  temp <- summarise(temp, heating_load = sum(heating_load))
+  temp$heating_load <- temp$heating_load/units
+  colnames(temp)[2] <- county
+  if(i == 1) final <- temp else final <- left_join(final, temp, by = "month")
+  print(i)
+}
+
+#sum(apply(final[,c(2:73)], 2, function(col){sum(col)}))/units
+
+counties <- colnames(final)[c(2:73)]
+counties <- str_remove(counties, "G550")
+counties <- paste0("55", str_remove(counties, "0$"))
+colnames(final)[c(2:73)] <- counties
+
+
+write.csv(final, "heating load by county.csv", row.names = F)
